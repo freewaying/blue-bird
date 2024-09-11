ccdcef8479b86f74f46a2c5bf844c09eb0bc1c98
freewaying <freewaying@gmail.com>
2024-09-11 12:20:24 +0800

    feat: Implement Protected Routes for Authenticated Users with Supabase Auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import AuthButtonServer from "./components/auth-button-server";
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  if (!session) {
+    redirect("/login");
+  }
   const { data: tweets } = await supabase.from("tweets").select();
   return (
     <>
