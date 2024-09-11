732d0a9444b1d8e0108852f82d80f64d23e93788
freewaying <freewaying@gmail.com>
2024-09-11 11:48:01 +0800

    feat: config github oauth to view all tweets

---
+++ app/components/auth-button-client.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import {
+  createClientComponentClient,
+  Session,
+} from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+
+export default function AuthButtonClient({
+  session,
+}: {
+  session: Session | null;
+}) {
+  const supabase = createClientComponentClient();
+  const router = useRouter();
+  const handleSignIn = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: `${location.origin}/auth/callback`,
+      },
+    });
+  };
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
+  };
+  return (
+    <div>
+      {session ? (
+        <button onClick={handleSignOut}>LoginOut</button>
+      ) : (
+        <button onClick={handleSignIn}>Login</button>
+      )}
+    </div>
+  );
+}
---
+++ app/middleware.ts
@@ -0,0 +1,11 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+  // refresh the session when it is expired
+  await supabase.auth.getSession();
+  return res;
+}
